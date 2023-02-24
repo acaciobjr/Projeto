48eca441966a44c86ad265af0b8ccce80ef85958
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-02-23 22:40:20 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,47 @@
-from flask import jsonify
+from flask import Flask, request, jsonify
+import psycopg2
 from service import UserService
 
-class UserController:
-    def get_all_users():
-        users = UserService.get_all_users()
-        return jsonify(users)
+#class UserController:
+ #   def get_all_users():
+  #      users = UserService.get_all_users()
+   #     return jsonify(users)
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="db",
+    user="postgres",
+    password="postgres"
+)
+
+#@app.route("/recursos", methods=["GET"])
+#def get_recursos():
+ #   cur = conn.cursor()
+  #  cur.execute("SELECT * FROM recursos")
+   # recursos = cur.fetchall()
+    #return jsonify([{ "id": r[0], "nome": r[1] } for r in recursos])
+
+@app.route("/recursos", methods=["POST"])
+def create_user(name, age, email, password):   
+    return jsonify({"name": name, "age": age, "email": email, "password": password})
+
+
+@app.route("/recursos/<int:id>", methods=["GET"])
+def read_user(users):    
+    if row in recurso:
+        return jsonify("Name: %s, Age: %s, Email: %s" % (row[0], row[1], row[2]))
+    elif recurso is None:
+        return "Recurso não encontrado", 404
+
+
+@app.route("/recursos/<int:id>", methods=["PUT"])
+def update_user(name, age, email, password):    
+    return jsonify({ "new name": name, "new age": age, "new email": email, "new password": password })
+
+@app.route("/recursos/<int:id>", methods=["DELETE"])
+def delete_user(users):
+    return "deleted feature", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
